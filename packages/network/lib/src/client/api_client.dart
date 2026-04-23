159e3765005a77e8f0307a8fee4ebb0c614448cd
hassanWork <h714241926@gmail.com>
2026-04-23 10:41:40 +0300

    add headers.

--- packages/network/lib/src/client/api_client.dart
+++ packages/network/lib/src/client/api_client.dart
@@ -4,6 +4,7 @@ abstract class ApiClient {
   Future<Result<T>> get<T>(
       String path, {
         Map<String, dynamic>? query,
+        Map<String, String>? headers,
         required JsonParser<T> parser,
       });
 
@@ -11,23 +12,27 @@ abstract class ApiClient {
       String path, {
         Map<String, dynamic>? body,
         Map<String, dynamic>? query,
+        Map<String, String>? headers,
         required JsonParser<T> parser,
       });
 
   Future<Result<T>> put<T>(
       String path, {
         Map<String, dynamic>? body,
+        Map<String, String>? headers,
         required JsonParser<T> parser,
       });
 
   Future<Result<T>> patch<T>(
       String path, {
         Map<String, dynamic>? body,
+        Map<String, String>? headers,
         required JsonParser<T> parser,
       });
 
   Future<Result<T>> delete<T>(
       String path, {
         required JsonParser<T> parser,
+        Map<String, String>? headers,
       });
 }
