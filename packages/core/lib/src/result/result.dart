0cb4c748e04ec66dacb969dec0b60b5c6fb5041c
hassan <h714241926@gmail.com>
2026-05-24 16:13:53 +0300

    add when to result.

--- packages/core/lib/src/result/result.dart
+++ packages/core/lib/src/result/result.dart
@@ -2,6 +2,19 @@ import '../errors/app_error.dart';
 
 sealed class Result<T> {
   const Result();
+
+  R when<R>({
+    required R Function(T data) success,
+    required R Function(AppError error) failure,
+  }) {
+    final self = this;
+    if (self is Success<T>) {
+      return success(self.data);
+    } else if (self is Failure<T>) {
+      return failure(self.error);
+    }
+    throw Exception('Unhandled Result type');
+  }
 }
 
 class Success<T> extends Result<T> {
