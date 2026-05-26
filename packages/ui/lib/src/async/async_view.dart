aa3983ebba51a7f5ee9f3dfdedad488ba6c95d29
hassan <h714241926@gmail.com>
2026-05-26 19:33:07 +0300

    set default values.

--- packages/ui/lib/src/async/async_view.dart
+++ packages/ui/lib/src/async/async_view.dart
@@ -6,36 +6,41 @@ class AsyncView<T> extends StatelessWidget {
   final T? data;
   final AppError? error;
 
-  final Widget Function() onLoading;
-  final Widget Function(AppError error) onError;
-  final Widget Function() onEmpty;
+  final Widget Function()? onLoading;
+  final Widget Function(AppError error)? onError;
+  final Widget Function()? onEmpty;
   final Widget Function(T data) onSuccess;
 
   const AsyncView({
     super.key,
     required this.status,
     required this.data,
-    required this.onLoading,
-    required this.onError,
-    required this.onEmpty,
     required this.onSuccess,
-    required this.error,
+    this.error,
+    this.onLoading,
+    this.onError,
+    this.onEmpty,
   });
 
   @override
   Widget build(BuildContext context) {
     switch (status) {
       case LoadStatus.loading:
-        return onLoading();
+        return onLoading != null
+            ? onLoading!()
+            : const Center(child: CircularProgressIndicator());
 
       case LoadStatus.error:
-        return onError(
-          error ?? const UnknownError('Something went wrong'),
-        );
+        final resolvedError = error ?? const UnknownError('Something went wrong');
+        return onError != null
+            ? onError!(resolvedError)
+            : Center(child: Text(resolvedError.toString()));
 
       case LoadStatus.success:
         if (data == null) {
-          return onEmpty();
+          return onEmpty != null
+              ? onEmpty!()
+              : const Center(child: Text('No data available'));
         }
         return onSuccess(data as T);
 
